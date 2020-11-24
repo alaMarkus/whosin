45ac4422728b1ba65bf4da3cf3030d72d3666788
Markus Alatalo <markusv.alatalo@hotmail.com>
2020-11-24 03:15:43 +0200

    path and build

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ const corsOptions = {
 }
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }else{
     app.use(cors(corsOptions))
