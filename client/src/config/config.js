4bc56ef2f90098d1f739a7cd20dddfb00945b691
Markus Alatalo <markusv.alatalo@hotmail.com>
2020-11-27 08:00:27 +0200

    fixed price

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,2 @@
-export const apiUrl = "http://localhost:8085"
-//export const apiUrl = "http://95.216.173.144:8085"
+//export const apiUrl = "http://localhost:8085"
+export const apiUrl = "http://95.216.173.144:8085"
